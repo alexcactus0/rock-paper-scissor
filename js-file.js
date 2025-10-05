4e434b55f21d9007f963be4d70ada572ef3e3475
alexcactus0 <217364473+alexcactus0@users.noreply.github.com>
2025-10-05 15:08:00 +0200

    Updated rps game

--- js-file.js
+++ js-file.js
@@ -1,16 +1,16 @@
-        
-        
+     
         const getComputerChoice = () => {
             const compDecision = document.querySelector(".comp");
             const options = ["rock", "paper", "scissor"];
             const randomOption = Math.floor(Math.random() * options.length);
             const choice = options[randomOption];
-            compDecision.textContent = ("Programm: " + choice);
+            compDecision.textContent = (choice);
             return choice;
         };
 
         let humanScore = 0;
         let computerScore = 0;
+        let round = 0;
 
         const playRound = (humanChoice, computerChoice) => {
             const decision = document.querySelector(".winnerboard");
@@ -28,8 +28,8 @@
               else { computerScore++;
                     decision.textContent = `You lost ${computerChoice} beats ${humanChoice}!`; }
         
-                humanScoreDiv.textContent = `Player: ${humanScore}`;
-                computerScoreDiv.textContent = `Computer: ${computerScore}`;
+                humanScoreDiv.textContent = `${humanScore}`;
+                computerScoreDiv.textContent = `${computerScore}`;
         };
 
         const humanScoreDiv = document.getElementById("player-score");
@@ -45,6 +45,20 @@
         paperBtn.addEventListener("click", () => callback("paper"));
         scissorBtn.addEventListener("click", () => callback("scissor"));
     };
+
+    const handleClick = (choice) => {
+        if (round < 5) {
+            round++;
+            const computerChoice = getComputerChoice();
+            callback(choice, computerChoice);
+
+            if (round === 5) {
+                setTimeout(() => {
+                    humanScore < computerScore ? 'You lost against a Computer skull emoji' : humanScore > computerScore ? 'Congrats you won!' : "It's a tie!";
+                }, 500)
+            }
+        }
+    }
 
         const playGame = () => {
                 humanSelection((playerChoice) => {
